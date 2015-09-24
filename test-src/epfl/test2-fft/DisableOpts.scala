3bd024c577517c823a3fe0cbb779f35c92c5c77b
Cédric Bastin <cedricbastin@gmail.com>
2015-09-24 11:37:14 -0700

    uncomment test1 ct

--- test-src/epfl/test2-fft/DisableOpts.scala
+++ test-src/epfl/test2-fft/DisableOpts.scala
@@ -1,16 +1,16 @@
-// package scala.lms
-// package epfl
-// package test2
+package scala.lms
+package epfl
+package test2
 
-// import internal._
+import internal._
 
-// trait DisableCSE extends Expressions {
-//   override def findDefinition[T](d: Def[T]) = None
-// }
+trait DisableCSE extends Expressions {
+  override def findDefinition[T](d: Def[T]) = None
+}
 
 
-// trait DisableDCE extends GraphTraversal {
-//   import IR._
-//   override def buildScheduleForResult(start: Any, sort: Boolean = true): List[Stm] =
-//     globalDefs
-// }
+trait DisableDCE extends GraphTraversal {
+  import IR._
+  override def buildScheduleForResult(start: Any, sort: Boolean = true): List[Stm] =
+    globalDefs
+}
