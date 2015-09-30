3ee0ad5d8b594ec04f6ba0570a50816172d9176f
Cédric Bastin <cedricbastin@gmail.com>
2015-09-30 00:35:32 -0700

    TestList passes

--- test-src/epfl/test12-collections/TestList.scala
+++ test-src/epfl/test12-collections/TestList.scala
@@ -1,70 +1,70 @@
-// package scala.lms
-// package epfl
-// package test12
+package scala.lms
+package epfl
+package test12
 
-// import common._
-// import java.io.PrintWriter
+import common._
+import java.io.PrintWriter
 
-// class TestList extends FileDiffSuite {
+class TestList extends FileDiffSuite {
 
-//   trait MapFlatMapAndFilter { this: ListOps with NumericOps with PrimitiveOps with OrderingOps =>
-//     def test(xs: Rep[List[Int]]): Rep[List[Int]] = {
-//       for {
-//         x <- xs
-//         y <- List(unit(1), unit(2), unit(3))
-//         if y < unit(3)
-//       } yield x * y
-//     }
-//   }
+  trait MapFlatMapAndFilter { this: ListOps with NumericOps with PrimitiveOps with OrderingOps =>
+    def test(xs: Rep[List[Int]]): Rep[List[Int]] = {
+      for {
+        x <- xs
+        y <- List(unit(1), unit(2), unit(3))
+        if y < unit(3)
+      } yield x * y
+    }
+  }
 
-//   trait Concat { this: ListOps =>
-//     def test(xs: Rep[List[Int]]): Rep[List[Int]] =
-//       xs ++ List(unit(1), unit(2), unit(3))
+  trait Concat { this: ListOps =>
+    def test(xs: Rep[List[Int]]): Rep[List[Int]] =
+      xs ++ List(unit(1), unit(2), unit(3))
 
-//     def emptyLeft(xs: Rep[List[Int]]): Rep[List[Int]] =
-//       List[Int]() ++ xs // VIRT 2.11: need type annotation
+    def emptyLeft(xs: Rep[List[Int]]): Rep[List[Int]] =
+      List[Int]() ++ xs // VIRT 2.11: need type annotation
 
-//     def emptyRight(xs: Rep[List[Int]]): Rep[List[Int]] =
-//       xs ++ List[Int]()
-//   }
+    def emptyRight(xs: Rep[List[Int]]): Rep[List[Int]] =
+      xs ++ List[Int]()
+  }
 
-//   trait MkString { this: ListOps =>
-//     def test(xs: Rep[List[Int]]): Rep[String] =
-//       xs.mkString
-//   }
+  trait MkString { this: ListOps =>
+    def test(xs: Rep[List[Int]]): Rep[String] =
+      xs.mkString
+  }
 
-//   val prefix = home + "test-out/epfl/test12-"
+  val prefix = home + "test-out/epfl/test12-"
 
-//   def testMapFlatMapAndFilter() {
-//     withOutFile(prefix+"map-flatmap-filter") {
-//       val prog = new MapFlatMapAndFilter with ListOpsExp with SeqOpsExp with ArrayOpsExp with NumericOpsExp with PrimitiveOpsExp with OrderingOpsExp
-//       val codegen = new ScalaGenEffect with ScalaGenListOps with ScalaGenNumericOps with ScalaGenPrimitiveOps with ScalaGenOrderingOps { val IR: prog.type = prog }
-//       import prog.{intTyp,unitTyp,listTyp}
-//       codegen.emitSource(prog.test, "MapFlatMapAndFilter", new PrintWriter(System.out))
-//     }
-//     assertFileEqualsCheck(prefix+"map-flatmap-filter")
-//   }
+  def testMapFlatMapAndFilter() {
+    withOutFile(prefix+"map-flatmap-filter") {
+      val prog = new MapFlatMapAndFilter with ListOpsExp with SeqOpsExp with ArrayOpsExp with NumericOpsExp with PrimitiveOpsExp with OrderingOpsExp
+      val codegen = new ScalaGenEffect with ScalaGenListOps with ScalaGenNumericOps with ScalaGenPrimitiveOps with ScalaGenOrderingOps { val IR: prog.type = prog }
+      import prog.{intTyp,unitTyp,listTyp}
+      codegen.emitSource(prog.test, "MapFlatMapAndFilter", new PrintWriter(System.out))
+    }
+    assertFileEqualsCheck(prefix+"map-flatmap-filter")
+  }
 
-//   def testConcat() {
-//     withOutFile(prefix+"concat") {
-//       val prog = new Concat with ListOpsExpOpt with SeqOpsExp with ArrayOpsExp with PrimitiveOpsExp
-//       val codegen = new ScalaGenEffect with ScalaGenListOps { val IR: prog.type = prog }
-//       import prog.{intTyp,unitTyp,listTyp}
-//       codegen.emitSource(prog.test, "Concat", new PrintWriter(System.out))
-//       codegen.emitSource(prog.emptyLeft, "ConcatEmptyLeft", new PrintWriter(System.out))
-//       codegen.emitSource(prog.emptyRight, "ConcatEmptyRight", new PrintWriter(System.out))
-//     }
-//     assertFileEqualsCheck(prefix+"concat")
-//   }
+  def testConcat() {
+    withOutFile(prefix+"concat") {
+      val prog = new Concat with ListOpsExpOpt with SeqOpsExp with ArrayOpsExp with PrimitiveOpsExp
+      val codegen = new ScalaGenEffect with ScalaGenListOps { val IR: prog.type = prog }
+      import prog.{intTyp,unitTyp,listTyp}
+      codegen.emitSource(prog.test, "Concat", new PrintWriter(System.out))
+      codegen.emitSource(prog.emptyLeft, "ConcatEmptyLeft", new PrintWriter(System.out))
+      codegen.emitSource(prog.emptyRight, "ConcatEmptyRight", new PrintWriter(System.out))
+    }
+    assertFileEqualsCheck(prefix+"concat")
+  }
 
-//   def testMkString() {
-//     withOutFile(prefix+"mkstring") {
-//       val prog = new MkString with ListOpsExp with SeqOpsExp with PrimitiveOpsExp
-//       val codegen = new ScalaGenEffect with ScalaGenListOps { val IR: prog.type = prog }
-//       import prog.{intTyp,unitTyp,listTyp,stringTyp}
-//       codegen.emitSource(prog.test, "MkString", new PrintWriter(System.out))
-//     }
-//     assertFileEqualsCheck(prefix+"mkstring")
-//   }
+  def testMkString() {
+    withOutFile(prefix+"mkstring") {
+      val prog = new MkString with ListOpsExp with SeqOpsExp with PrimitiveOpsExp
+      val codegen = new ScalaGenEffect with ScalaGenListOps { val IR: prog.type = prog }
+      import prog.{intTyp,unitTyp,listTyp,stringTyp}
+      codegen.emitSource(prog.test, "MkString", new PrintWriter(System.out))
+    }
+    assertFileEqualsCheck(prefix+"mkstring")
+  }
 
-// }
+}
